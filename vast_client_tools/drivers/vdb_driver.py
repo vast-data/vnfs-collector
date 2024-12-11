9456c69407e7035e79b3269e81da45c6de788011
Sagi Grimberg <sagi@grimberg.me>
2024-12-11 14:23:42 +0200

    vdb: keep vdb sessions short lived
    Open a new session for each sample, and then tear it down.
    The main reason is that we want as much spreading of collectors
    to different cnodes as possible.
    
    This will not be very efficient for static vips, but for a DNS name
    which is the intended use, we want that each sample will resolve to
    a different cnode.
    
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>

--- vast_client_tools/drivers/vdb_driver.py
+++ vast_client_tools/drivers/vdb_driver.py
@@ -30,33 +30,35 @@ class VdbDriver(DriverBase):
 
         args = await super().setup(args, namespace)
         self.db_endpoint = args.db_endpoint.strip("http://").strip("https://")
-        db_access_key = args.db_access_key
-        db_secret_key = args.db_secret_key
+        self.db_access_key = args.db_access_key
+        self.db_secret_key = args.db_secret_key
         self.db_bucket = args.db_bucket
         self.db_schema = args.db_schema
         self.db_table = args.db_table
         self.db_ssl_verify = args.db_ssl_verify
-        self.vastapi = VastdbApi(
-            host=self.db_endpoint, access_key=db_access_key, secret_key=db_secret_key, secure=self.db_ssl_verify
-        )
-        if not self.vastapi.list_schemas(bucket=self.db_bucket, schema=self.db_schema):
+        vastapi = VastdbApi(host=self.db_endpoint, access_key=self.db_access_key,
+                            secret_key=self.db_secret_key, secure=self.db_ssl_verify)
+        if not vastapi.list_schemas(bucket=self.db_bucket, schema=self.db_schema):
             raise ValueError(f"Schema {self.db_schema} does not exist.")
 
-        _, _, tables, *_ = self.vastapi.list_tables(
+        _, _, tables, *_ = vastapi.list_tables(
             bucket=self.db_bucket, schema=self.db_schema, name_prefix=self.db_table, exact_match=True
         )
         if not tables:
             raise ValueError(f"Table {self.db_table} does not exist.")
 
-        columns, *_ = self.vastapi.list_columns(bucket=self.db_bucket, schema=self.db_schema, table=self.db_table)
+        columns, *_ = vastapi.list_columns(bucket=self.db_bucket, schema=self.db_schema, table=self.db_table)
         fields = [pa.field(name, dtype) for name, dtype, *_ in columns]
         self.arrow_schema = pa.schema(fields)
         self.logger.info(f"{self} has been initialized.")
+        del vastapi
 
     async def store_sample(self, data):
         df_copy = data.copy()
         df_copy['TAGS'] = df_copy['TAGS'].apply(lambda d: list(d.items()))
         record_batch = pa.RecordBatch.from_pandas(df=df_copy, schema=self.arrow_schema)
-        self.vastapi.insert(
-            bucket=self.db_bucket, schema=self.db_schema, table=self.db_table, record_batch=record_batch
-        )
+        vastapi = VastdbApi(host=self.db_endpoint, access_key=self.db_access_key,
+                            secret_key=self.db_secret_key, secure=self.db_ssl_verify)
+        vastapi.insert(bucket=self.db_bucket, schema=self.db_schema, table=self.db_table,
+                       record_batch=record_batch)
+        del vastapi
