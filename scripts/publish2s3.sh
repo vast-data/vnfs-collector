6d01f6d9d0c81d20ba6e2e2027fe960a0d0881e2
Sagi Grimberg <sagi@grimberg.me>
2024-12-27 13:31:28 +0200

    s3: upload artifacts to s3 bucket
    After make, we simply upload to s3 the rpm, deb and update
    the meta.json in our public s3 bucket: s3://vast-client-metrics
    
    Then customers can download by looking at the latest version in:
    https://vast-client-metrics.s3.eu-west-1.amazonaws.com/meta.json
    
    And download the packages via:
    $ wget
    https://vast-client-metrics.s3.eu-west-1.amazonaws.com/vnfs-collector-[$latest].noarch.rpm
    https://vast-client-metrics.s3.eu-west-1.amazonaws.com/vnfs-collector_[$latest]_all.deb
    
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>

---
+++ scripts/publish2s3.sh
@@ -0,0 +1,18 @@
+#~/bin/bash
+version=`cat version.txt`-`git rev-parse --short HEAD`
+
+rpmfile=vnfs-collector-${version}.noarch.rpm
+debfile=vnfs-collector_${version}_all.deb
+echo s3cmd put dist/$rpmfile s3://vast-client-metrics/$rpmfile
+s3cmd put dist/$rpmfile s3://vast-client-metrics/$rpmfile
+echo s3cmd put dist/$debfile s3://vast-client-metrics/$debfile
+s3cmd put dist/$debfile s3://vast-client-metrics/$debfile
+if [ "$1" == "latest" ]; then
+	meta=$(mktemp)
+	echo "{'latest': '$version'}" > $meta
+	echo "meta: `cat $meta`"
+	echo s3cmd put meta.json s3://vast-client-metrics/meta.json
+	s3cmd put $meta s3://vast-client-metrics/meta.json
+fi
+
+
